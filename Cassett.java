e3c6862f15264da243d9e0389c0079d3f146a975
Rachael Stokes <stokesrachaele@gmail.com>
2021-04-20 14:29:56 -0400

    added driver and code to cassett

--- Cassett.java
+++ Cassett.java
@@ -1,3 +1,39 @@
-public class Cassett {
+import java.util.*;
+public class Cassett implements AnalogAlbum {
+    private ArrayList<String> songs = new ArrayList<String>();
+    int currSong = 0; //index of the current song
+
+    public Cassett(String song1, String song2, String song3, String song4, String song5) {
+        songs.add(song1);
+        songs.add(song2);
+        songs.add(song3);
+        songs.add(song4);
+        songs.add(song5);
+    }
+
+    public String play() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String rewind() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String ffwd() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String pause() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String stopEject() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
